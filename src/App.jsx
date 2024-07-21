46458c7785d627c8fd636576b6cb05af2c24ee03
brainbotsector <88769793+brainbotsector@users.noreply.github.com>
2024-07-21 18:04:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReferEarn from './ReferEarn';
+import { ThemeProvider, ThemeContext } from './ThemeContext'; 
 import './App.css';
 
 function App() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  if (!theme) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <ReferEarn />
-    </div>
+    <ThemeProvider>
+      <div className={`App ${theme.background === '#000' ? 'dark-theme' : ''}`}>
+        <ReferEarn />
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </div>
+    </ThemeProvider>
   );
 }
 
